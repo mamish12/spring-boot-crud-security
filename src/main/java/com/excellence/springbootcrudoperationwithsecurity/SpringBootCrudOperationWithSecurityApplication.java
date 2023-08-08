b261e632d550d98972bbd73ca276143b8a405e85
mamish12 <mku20sahu@gmail.com>
2023-08-08 10:20:18 +0530

    first commit

---
+++ src/main/java/com/excellence/springbootcrudoperationwithsecurity/SpringBootCrudOperationWithSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.excellence.springbootcrudoperationwithsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudOperationWithSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudOperationWithSecurityApplication.class, args);
+	}
+
+}
