c3cd2cc1fdf94512e8dcd623c05096f0ab4e8c42
mamish12 <mku20sahu@gmail.com>
2023-08-14 16:40:36 +0530

    added stsout

--- src/main/java/com/excellence/springbootcrudapisecurity/SpringBootCrudApiSecurityApplication.java
+++ src/main/java/com/excellence/springbootcrudapisecurity/SpringBootCrudApiSecurityApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootCrudApiSecurityApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello world");
 		SpringApplication.run(SpringBootCrudApiSecurityApplication.class, args);
 	}
 
